8d7f2c44ec38eb1ef59c531d3cd1ef63a95e3c5e
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-09-13 13:33:09 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚡ IMPORTANT: change 'AI-Education' to match your repo name
+// ⚡ MUST match the GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/AI-Education/', 
+  base: '/AISCHOOL2/', 
 })
